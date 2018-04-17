e7b3bebd0ef0eea9ff683480944fbb2275b6839f
DESKTOP-VE45OLC\debbi <hempel-costello.simon20@libertycommon.org>
2018-04-16 18:00:22 -0600

    Simon:added better drive code to colorado regional end code

--- src/org/usfirst/frc/team4499/robot/commands/TeleopDriving.java
+++ src/org/usfirst/frc/team4499/robot/commands/TeleopDriving.java
@@ -13,6 +13,8 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class TeleopDriving extends Command {
 	public boolean highGear = false;
+	private double leftPower = 0;
+	private double rightPower= 0;
     public TeleopDriving() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
@@ -27,29 +29,58 @@ public class TeleopDriving extends Command {
     	double leftJoystick = OI.joyStickOne.getRawAxis(1);
     	double rightJoystick = OI.joyStickOne.getRawAxis(5);
     	
+
+		
     	if(highGear) {
-    		leftJoystick *= 0.9;
-    		rightJoystick *= 0.9;
-    	}
-    	
-    	if(Math.abs(OI.joyStickOne.getRawAxis(1))>0) {
-        	double leftPower = Math.pow(Math.abs(leftJoystick),2)*Math.abs(leftJoystick)/leftJoystick;
-        	
+    		if(Math.abs(OI.joyStickOne.getRawAxis(1))>0.15) {
+       			leftPower = Math.pow(Math.abs(leftJoystick),2)*Math.abs(leftJoystick)/leftJoystick;
+       		}
+       		else {
+    			RobotMap.leftDriveLead.set(ControlMode.PercentOutput, 0);
+    			leftPower = 0;
+    		}
+       		if(Math.abs(OI.joyStickOne.getRawAxis(5))>0.15) {
+    			rightPower = Math.pow(Math.abs(rightJoystick),2)*Math.abs(rightJoystick)/rightJoystick;
+    		}
+    		else {
+    			rightPower = 0;
+    			RobotMap.rightDriveLead.set(ControlMode.PercentOutput, 0); 
+    		}
+       		if(Math.abs(leftPower)>Math.abs(rightPower)&&Math.abs(leftPower-rightPower)<=1) {
+       			double dif = leftPower - rightPower;
+       			rightPower = leftPower-((dif)*Math.abs(dif));
+       		}
+       		else if(Math.abs(rightPower)>Math.abs(leftPower)&&Math.abs(rightPower-leftPower)<=1) {
+       			double dif = rightPower - leftPower;
+       			leftPower = rightPower-((dif)*Math.abs(dif));
+       		}
+    		RobotMap.rightDriveLead.set(ControlMode.PercentOutput, rightPower);
     		RobotMap.leftDriveLead.set(ControlMode.PercentOutput, leftPower);
     		
     	}
     	else {
-    		RobotMap.leftDriveLead.set(ControlMode.PercentOutput, 0);
-    	}
-    	if(Math.abs(OI.joyStickOne.getRawAxis(5))>0) {
-    		double rightPower = Math.pow(Math.abs(rightJoystick),2)*Math.abs(rightJoystick)/rightJoystick;
-    	    RobotMap.rightDriveLead.set(ControlMode.PercentOutput, rightPower);
-    	}
-    	else {
-    		
-    		RobotMap.rightDriveLead.set(ControlMode.PercentOutput, 0); 
+    		if(Math.abs(OI.joyStickOne.getRawAxis(1))>0.15){
+    			RobotMap.leftDriveLead.set(ControlMode.PercentOutput, OI.joyStickOne.getRawAxis(1));
+    		}
+    		else {
+    			RobotMap.leftDriveLead.set(ControlMode.PercentOutput, 0);
+
+    		}
+    		if(Math.abs(OI.joyStickOne.getRawAxis(5))>0.15){
+    			RobotMap.rightDriveLead.set(ControlMode.PercentOutput, OI.joyStickOne.getRawAxis(5));
+    		}
+    		else {
+    			RobotMap.rightDriveLead.set(ControlMode.PercentOutput, 0);
+
+    		}	
     	}
     	
+
+    
+	
+
+
+    		
     	if(OI.shiftUp.get()) {
     		highGear = true;
     		RobotMap.shifters.set(RobotMap.highGear);
