881bb8e45d3479145171d8e8f704d37d01ed5889
DESKTOP-VE45OLC\debbi <hempel-costello.simon20@libertycommon.org>
2018-06-08 16:48:20 -0600

    Better Sequencing

--- src/org/usfirst/frc/team4499/robot/autocommands/MultiCubeAutoSwitch.java
+++ src/org/usfirst/frc/team4499/robot/autocommands/MultiCubeAutoSwitch.java
@@ -34,8 +34,8 @@ public class MultiCubeAutoSwitch extends CommandGroup {
     		addParallel(new MPArm(120,10));
     		addSequential(new motionMagicDriveForwardHighGear(37,RobotMap.navx.getAngle(),3050,5000,1,1));
     		addSequential(new SlipTurn(-40,0.75f,0.6));
-    		addSequential(new motionMagicDriveForwardHighGear(10,RobotMap.navx.getAngle(),3050,5000,1,1));
     		addParallel(new MPArm(140,10));
+    		addSequential(new motionMagicDriveForwardHighGear(10,RobotMap.navx.getAngle(),3050,5000,1,1));
             addSequential(new OutTakeCrate(0.4,0.4));
     	}
     	else if(Dir =='L') {
